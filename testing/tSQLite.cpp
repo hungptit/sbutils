ea5f03acc7c1e5e36d4eb866d06a88ac93e8c4f7
Hung Dang <hungptit@gmail.com>
2015-05-06 21:59:48 -0400

    Fix tSQLite example.

--- testing/tSQLite.cpp
+++ testing/tSQLite.cpp
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-#include "Poco/Data/Session.h"
-#include "Poco/Data/SQLite/Connector.h"
-#include <vector>
-#include <iostream>
-
-=======
 #include "Poco/Data/Data.h"
 #include "Poco/Data/Session.h"
 #include "Poco/Data/Extraction.h"
@@ -15,9 +8,6 @@
 #include <vector>
 #include <iostream>
 
-using namespace Poco::Data;
-
->>>>>>> 877f4143259cbeccc23bed2b9d44c00ddcadd83d
 struct Person {
     std::string name;
     std::string address;
@@ -25,24 +15,21 @@ struct Person {
 };
 
 int main() {
-<<<<<<< HEAD
-    using namespace Poco::Data;
-=======
->>>>>>> 877f4143259cbeccc23bed2b9d44c00ddcadd83d
-    // register SQLite connector
-    Poco::Data::SQLite::Connector::registerConnector();
-
-    // create a session
+  using namespace Poco::Data::Keywords;
+  
+  // register SQLite connector
+  Poco::Data::SQLite::Connector::registerConnector();
+  
+  // create a session
     Poco::Data::Session session("SQLite", "sample.db");
 
-    // drop sample table, if it exists
-<<<<<<< HEAD
-    session << "DROP TABLE IF EXISTS Person", Poco::Data::now;
+    // // drop sample table, if it exists
+    session << "DROP TABLE IF EXISTS Person", now;
 
     // (re)create table
     session << "CREATE TABLE Person (Name VARCHAR(30), Address VARCHAR, Age "
                "INTEGER(3))",
-        Poco::Data::now;
+        now;
 
     // insert some rows
     Person person = {"Bart Simpson", "Springfield", 12};
@@ -70,41 +57,6 @@ int main() {
         std::cout << person.name << " " << person.address << " " << person.age
                   << std::endl;
     }
-=======
-    // session << "DROP TABLE IF EXISTS Person", Poco::Data::now;
-
-    // // (re)create table
-    // session << "CREATE TABLE Person (Name VARCHAR(30), Address VARCHAR, Age "
-    //            "INTEGER(3))",
-    //     Poco::Data::now;
-
-    // // insert some rows
-    // Person person = {"Bart Simpson", "Springfield", 12};
-
-    // Statement insert(session);
-    // insert << "INSERT INTO Person VALUES(?, ?, ?)", use(person.name),
-    //     use(person.address), use(person.age);
-
-    // insert.execute();
-
-    // person.name = "Lisa Simpson";
-    // person.address = "Springfield";
-    // person.age = 10;
-
-    // insert.execute();
-
-    // // a simple query
-    // Statement select(session);
-    // select << "SELECT Name, Address, Age FROM Person", into(person.name),
-    //     into(person.address), into(person.age),
-    //     range(0, 1); //  iterate over result set one row at a time
-
-    // while (!select.done()) {
-    //     select.execute();
-    //     std::cout << person.name << " " << person.address << " " << person.age
-    //               << std::endl;
-    // }
->>>>>>> 877f4143259cbeccc23bed2b9d44c00ddcadd83d
 
     return 0;
 }
