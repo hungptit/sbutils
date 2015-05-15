9d93b7929224492982159cf9bfcd820886df8a1e
Hung Dang <hung.dang@mathworks.com>
2015-05-15 12:34:19 -0400

    Add the finder and viewer which works with leveldb database.

--- FileFinder.hpp
+++ FileFinder.hpp
@@ -12,6 +12,70 @@
 #include "leveldb/cache.h"
 
 namespace Tools {
+    class Writer {
+      public:
+        Writer(const std::string &dataFile) : DataFile(dataFile) {
+            leveldb::Options options;
+            // options.cache = leveldb::NewLRUCache(100 * 1048576);  // 100MB cache
+            options.create_if_missing = true;
+            leveldb::Status status = leveldb::DB::Open(options, DataFile, &Database);
+
+            // Check that the database is opened successfully.
+            if (false == status.ok()) {
+                std::cerr << "Unable to open/create database" << DataFile << std::endl;
+                std::cerr << status.ToString() << std::endl;
+            }
+        }
+
+        ~Writer() { delete Database; }
+
+        template <typename Container> void write(Container &data) {
+            leveldb::WriteOptions writeOptions;
+            for (const auto &val : data) {
+                // TODO: Use template the write out the data.
+                Database->Put(writeOptions, std::get<0>(val), boost::lexical_cast<std::string>(std::get<1>(val)));
+            }
+        }
+        
+      private:
+        leveldb::DB *Database;
+        std::string DataFile;
+    };
+
+    class Reader {
+      public:
+        Reader(const std::string &dataFile) : DataFile(dataFile) {
+            leveldb::Options options;
+            options.create_if_missing = false;
+            leveldb::Status status = leveldb::DB::Open(options, DataFile, &Database);
+            if (false == status.ok()) {
+                std::cerr << "Unable to open database" << DataFile << std::endl;
+                std::cerr << status.ToString() << std::endl;
+            }
+        }
+
+        void read() {
+            leveldb::Iterator *it = Database->NewIterator(leveldb::ReadOptions());
+
+            for (it->SeekToFirst(); it->Valid(); it->Next()) {
+                std::cout << it->key().ToString() << " : " << it->value().ToString() << std::endl;
+            }
+
+            if (false == it->status().ok()) {
+                std::cerr << "An error was found during the scan" << std::endl;
+                std::cerr << it->status().ToString() << std::endl;
+            }
+
+            delete it;
+        }
+
+        ~Reader() { delete Database; }
+
+      private:
+        std::string DataFile;
+        leveldb::DB *Database;
+    };
+
     /**
      * This class will search for files/folders and write to the output stream
      * which can be files, databases, or stdout.
@@ -19,12 +83,7 @@ namespace Tools {
     class Finder {
       public:
         typedef std::tuple<std::string, boost::filesystem::perms, std::time_t> value_type;
-
-        /**
-         * Build the file database of the current folder.
-         *
-         * @param searchPath
-         */
+        
         void search(const boost::filesystem::path &searchPath) {
             boost::filesystem::recursive_directory_iterator endIter;
             boost::filesystem::recursive_directory_iterator dirIter(searchPath);
@@ -38,39 +97,10 @@ namespace Tools {
                 }
             }
         }
-
+        
         std::vector<value_type> &getData() { return Data; }
-
+        
         void reset() { Data.clear(); }
-
-        // TODO: Move this method out of this class
-        bool write(const std::string &database) {
-            leveldb::Options options;
-            // options.cache = leveldb::NewLRUCache(100 * 1048576);  // 100MB cache
-            options.create_if_missing = true;
-
-            // Open the database
-            leveldb::DB *db;
-            leveldb::Status status = leveldb::DB::Open(options, database, &db);
-
-            if (!status.ok())
-                std::cerr << status.ToString() << std::endl;
-
-            if (false == status.ok()) {
-                std::cerr << "Unable to open/create database" << database << std::endl;
-                std::cerr << status.ToString() << std::endl;
-                return false;
-            }
-
-            leveldb::WriteOptions writeOptions;
-            for (const auto &val : Data) {
-                db->Put(writeOptions, std::get<0>(val), boost::lexical_cast<std::string>(std::get<1>(val)));
-            }
-
-            // Close the database
-            delete db;
-            return true;
-        }
 
       private:
         std::vector<value_type> Data;
--- testing/viewer.cpp
+++ testing/viewer.cpp
@@ -15,35 +15,26 @@ int main (int argc, char * argv[]) {
     desc.add_options()
         ("help,h", "finder - Find all files of a given folder and .")
         ("verbose,v", "Display all data.")
-        ("folder,d", po::value<std::vector<std::string>>(), "Search folders.")      
         ("database,o", po::value<std::string>(), "An output database.");
 
     po::positional_options_description p;
-    p.add("folder", -1);
+    p.add("database", -1);
     po::variables_map vm;
     po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(),
               vm);
     po::notify(vm);
 
     if (vm.count("help")) {
-        std::cout << "Usage: finder [options]\n";
+        std::cout << "Usage: viewer [options]\n";
         std::cout << desc;
         std::cout << "Examples:" << std::endl;
         std::cout
-            << "\t finder ./ -o testdata."
+            << "\t viewer testdata"
             << std::endl;
         return 0;
     }
 
-    // Process input parameters
-    std::vector<std::string> folders;
     std::string database ;
-
-    if (vm.count("folder")) {
-        folders = vm["folder"].as<std::vector<std::string>>();
-    } else {
-        folders.emplace_back("./");
-    }
 
     if (vm.count("database")) {
         database = vm["database"].as<std::string>();
@@ -51,12 +42,9 @@ int main (int argc, char * argv[]) {
         database = "data";
     }
 
-    // Build file information database
-    Tools::Finder fSearch;
-    for (const auto & val : folders) {
-        fSearch.search(val);
-    }
-    Tools::Writer writer(database);
-    writer.write(fSearch.getData());    
+    // Read the database
+    Tools::Reader reader(database);
+    reader.read();
+    
     return 0;
 }
