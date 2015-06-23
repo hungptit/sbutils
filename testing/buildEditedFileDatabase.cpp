5ba1c9667adc14b540b736348e708645e5f8dbf1
Hung Dang <hung.dang@mathworks.com>
2015-06-23 10:08:56 -0400

    Add build*Database commands.

---
+++ testing/buildEditedFileDatabase.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <array>
+#include <tuple>
+#include "boost/filesystem.hpp"
+#include "utils/Utils.hpp"
+#include "utils/FindUtils.hpp"
+#include "utils/LevelDBIO.hpp"
+#include "boost/program_options.hpp"
+#include "InputArgumentParser.hpp"
+
+int main(int argc, char *argv[]) {
+    Tools::InputArgumentParser params(argc, argv);
+    if (params.Verbose) {
+        params.disp();
+    }
+
+    // Build file information database
+    Tools::BuildEditedFileDataBase<Tools::Finder<Tools::ExtendedFileInfo>> fSearch;
+    for (const auto &val : params.Folders) {
+        fSearch.search(val);
+    }
+
+    Tools::print(fSearch.getData());
+    
+    // Write results to database.
+    Tools::Writer writer(params.Database);
+    // writer.write(fSearch.getData());
+
+    return 0;
+}
