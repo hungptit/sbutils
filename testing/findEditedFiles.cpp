44498cb839c3774f0056b697f9add51db6522340
Hung Dang <hung.dang@mathworks.com>
2015-06-30 15:42:42 -0400

    1. Remove unused code 2. Update findEditedFiles using lambda.

--- testing/findEditedFiles.cpp
+++ testing/findEditedFiles.cpp
@@ -4,13 +4,16 @@
 #include <vector>
 #include <array>
 #include <tuple>
-#include <unordered_map>
+
 #include "boost/filesystem.hpp"
 #include "boost/unordered_map.hpp"
+#include "boost/unordered_set.hpp"
+#include "boost/program_options.hpp"
+
 #include "utils/Utils.hpp"
 #include "utils/FindUtils.hpp"
 #include "utils/LevelDBIO.hpp"
-#include "boost/program_options.hpp"
+
 #include "InputArgumentParser.hpp"
 
 #include <string>
@@ -41,9 +44,9 @@ std::vector<FileInfo> getEditedFiles(const Tools::InputArgumentParser &params) {
     return data;
 }
 
-boost::unordered_map<std::string, FileInfo> load(const Tools::InputArgumentParser &params) {
+boost::unordered_map<std::string, FileInfo> loadDatabase(const Tools::InputArgumentParser &params) {
     std::vector<FileInfo> database;
-    database.reserve(1200000);  // Optimized for the MW sandbox
+    database.reserve(1200000); // Optimized for the MW sandbox
     if (!params.Database.empty()) {
         Tools::Reader reader(params.Database);
 
@@ -81,17 +84,15 @@ boost::unordered_map<std::string, FileInfo> load(const Tools::InputArgumentParse
     return lookupTable;
 }
 
-std::vector<FileInfo> filter(boost::unordered_map<std::string, FileInfo> &lookupTable,
-                             std::vector<FileInfo> &data) {
+std::vector<FileInfo> filter(boost::unordered_map<std::string, FileInfo> &lookupTable, std::vector<FileInfo> &data) {
     std::vector<FileInfo> editedFiles;
     for (const auto &anEditedFile : data) {
         auto aKey = std::get<0>(anEditedFile);
         auto aFile = lookupTable.find(aKey);
-        if ((aFile != lookupTable.end()) && (anEditedFile != aFile->second)) {
-            // std::cout << "--------------" << std::endl;
-            // std::cout << aKey << std::endl;
-            // std::cout << anEditedFile << std::endl;
-            // std::cout << aFile->second << std::endl;
+        if ((aFile == lookupTable.end()) || (anEditedFile != aFile->second)) {
+            // If we could not find a given key in the database or the value
+            // associated with that key is the the same with the current value
+            // then we need to record this file.
             editedFiles.emplace_back(anEditedFile);
         }
     }
@@ -105,8 +106,21 @@ void print(const Tools::InputArgumentParser &params, std::vector<FileInfo> &edit
             std::cout << val << std::endl;
         }
     } else {
+        // TODO: These parameters are specific to the MW sandbox so we need to
+        // store them in database?
+        std::array<std::string, 3> excludedStems = {{"/.sbtools/", "/derived/", "toolbox_cache-glnxa64"}};
+        std::array<std::string, 1> excludedExtensions = {{".p"}};
         for (const auto &val : editedFiles) {
-            std::cout << std::get<0>(val) << std::endl;
+            bool isExcluded = false;
+            auto aPath = std::get<0>(val);
+            isExcluded = std::any_of(excludedExtensions.begin(), excludedExtensions.end(),
+                                     [=](const std::string &extStr) { return extStr == std::get<2>(val); }) ||
+                         std::any_of(excludedStems.begin(), excludedStems.end(),
+                                     [=](const std::string &extStr) { return aPath.find(extStr) != std::string::npos; });
+
+            if (!isExcluded) {
+                std::cout << std::get<0>(val) << std::endl;
+            }
         }
     }
 }
@@ -123,8 +137,8 @@ int main(int argc, char *argv[]) {
     // If help command is specified then we won't search for edited files.
     if (!params.Help) {
         // TODO: Use async programming model for this code segment.
-        auto data = getEditedFiles(params);                   // Get the list of edited files.
-        auto lookupTable = load(params);                      // Load the lookup table from the database
+        auto data = getEditedFiles(params);           // Get the list of edited files.
+        auto lookupTable = loadDatabase(params);      // Load the lookup table from the database
         auto editedFiles = filter(lookupTable, data); // Filter edited file list
         print(params, editedFiles);
     }
