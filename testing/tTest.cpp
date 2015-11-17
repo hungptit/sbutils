d175fc44f4e719700a7dee8b36daa30830822690
Hung Dang <hungptit@gmail.com>
2015-11-17 18:32:18 -0500

    Start the extend the DFSSearch with more complicated filters.

--- testing/tTest.cpp
+++ testing/tTest.cpp
@@ -46,7 +46,7 @@ namespace Tools {
                     auto ftype = status.type();
                     if (ftype == boost::filesystem::regular_file) {
                         if (isValidFile(currentPath)) {
-                            update(currentPath);
+                            update(currentPath, status);
                             counter++;
                         }
                     } else if (ftype == boost::filesystem::directory_file) {
@@ -64,56 +64,40 @@ namespace Tools {
       protected:
         virtual bool isValidDir(const path &aPath) = 0;
         virtual bool isValidFile(const path &aPath) = 0;
-        virtual void update(const path &aPath) = 0;
+        virtual void update(const path &aPath,
+                            boost::filesystem::file_status &fs) = 0;
         virtual void unexpected(const path &aPath) = 0;
     };
 
     template <typename Base> class SimpleDFSFinder : public Base {
       public:
+        auto getData() { return Data; }
+
       protected:
         typedef typename Base::path path;
-        bool isValidDir(const path &) { return true; };
+        bool isValidDir(const path &aPath) {
+            const std::array<std::string, 1> excludedFolders = {{".git"}};
+            auto searchStr = aPath.extension().string();
+            return std::find(excludedFolders.begin(), excludedFolders.end(),
+                             searchStr) == excludedFolders.end();
+        };
         bool isValidFile(const path &) { return true; };
-        void update(const path &){};
+
+        void update(const path &aPath, boost::filesystem::file_status &fs) {
+            Data.emplace_back(
+                std::make_tuple(aPath.string(), aPath.stem().string(),
+                                aPath.extension().string(), fs.permissions(),
+                                boost::filesystem::last_write_time(aPath)));
+        };
         void unexpected(const path &){};
 
       private:
-    };
-
-    template <typename Base> class TestFinder : public Base {
-      public:
-        ~TestFinder() { std::cout << counter << std::endl; }
-
-        void update(typename Base::iter_type &dirIter) {
-            const boost::filesystem::file_status fs = dirIter->status();
-            if ((fs.type() == boost::filesystem::regular_file)) {
-                counter++;
-            }
-        }
-
-      private:
-        size_t counter = 0;
+        std::vector<Tools::EditedFileInfo> Data;
     };
 }
 
 int main(int argc, char *argv[]) {
-    // std::cout << argv[1] << "\n";
-    // std::string fName(argv[1]);
-    // std::cout << fName << "\n";
-
-    // boost::filesystem::path aPath("/home/hungptit/projects/3p");
     boost::filesystem::path aPath(argv[1]);
-    // std::cout << boost::filesystem::is_directory(aPath) << "\n";
-
-    {
-        Timer timer;
-        Tools::TestFinder<Tools::Finder> finder;
-        finder.search(aPath);
-        // std::cout << "Number of file: " << finder.search(aPath) <<
-        // std::endl;;
-        std::cout << "Total time: " << timer.toc() / timer.ticksPerSecond()
-                  << " seconds" << std::endl;
-    }
 
     {
         Timer timer;
@@ -121,15 +105,8 @@ int main(int argc, char *argv[]) {
         std::cout << "Number of file: " << finder.search(aPath) << std::endl;
         std::cout << "Total time: " << timer.toc() / timer.ticksPerSecond()
                   << " seconds" << std::endl;
-    }
-
-    {
-        Timer timer;
-        Tools::TestFinder<Tools::Finder> finder;
-        finder.search(aPath);
-        // std::cout << "Number of file: " << finder.search(aPath) <<
-        // std::endl;;
-        std::cout << "Total time: " << timer.toc() / timer.ticksPerSecond()
-                  << " seconds" << std::endl;
+        // for (auto aFile : finder.getData()) {
+        //     std::cout << aFile << "\n";
+        // }
     }
 }
