92b44818b95c53e6fcaf280a98c1147be6451da1
hungdang <hungptit@gmail.com>
2015-11-19 16:54:29 -0500

    Add file search implementation using BFS.

---
+++ BFSFileSearch.hpp
@@ -0,0 +1,72 @@
+#ifndef BFSFileSearch_hpp_
+#define BFSFileSearch_hpp_
+
+// STL headers
+#include <string>
+#include <tuple>
+#include <array>
+#include <vector>
+#include <queue>
+#include <deque>
+
+// Boost libraries
+#define BOOST_THREAD_VERSION 4
+#include "boost/config.hpp"
+#include "boost/filesystem.hpp"
+
+// Headers from utils package.
+#include "utils/Utils.hpp"
+
+// CPPFormat
+#include "cppformat/format.h"
+
+namespace Utils {
+    /**
+     * This class define a file search algorithm using the depth-first search. 
+     * Derived class will need to overwrite virtual functions.
+     */
+    class BFSFileSearchBase {
+      public:
+        typedef boost::filesystem::path path;
+        typedef boost::filesystem::directory_iterator directory_iterator;
+
+        size_t search(const path &aPath) {
+            size_t counter = 0;
+            std::deque<path> folders;
+            folders.push_back(aPath);
+            while (!folders.empty()) {
+                auto aPath = folders.front();
+                folders.pop_front();
+                directory_iterator endIter;
+                directory_iterator dirIter(aPath);
+                for (; dirIter != endIter; ++dirIter) {
+                    auto currentPath = dirIter->path();
+                    auto status = dirIter->status();
+                    auto ftype = status.type();
+                    if (ftype == boost::filesystem::regular_file) {
+                        if (isValidFile(currentPath)) {
+                            update(currentPath, status);
+                            counter++;
+                        }
+                    } else if (ftype == boost::filesystem::directory_file) {
+                        if (isValidDir(currentPath)) {
+                            folders.push_back(currentPath);
+                        }
+                    } else {
+                        unexpected(currentPath);
+                    }
+                }
+            }
+            return counter;
+        }
+
+      protected:
+        virtual bool isValidDir(const path &aPath) = 0;
+        virtual bool isValidFile(const path &aPath) = 0;
+        virtual void update(const path &aPath,
+                            boost::filesystem::file_status &fs) = 0;
+        virtual void unexpected(const path &aPath) = 0;
+    };    
+}
+
+#endif
--- testing/tTest.cpp
+++ testing/tTest.cpp
@@ -20,90 +20,32 @@
 #include "utils/LevelDBIO.hpp"
 #include "utils/Timer.hpp"
 #include "utils/Utils.hpp"
+#include "utils/DFSFileSearch.hpp"
+#include "utils/BFSFileSearch.hpp"
+#include "utils/FileSearch.hpp"
 
 #include <sstream>
 #include <string>
 #include <vector>
 
-namespace Tools {
-    class DFSFinder {
-      public:
-        typedef boost::filesystem::path path;
-        typedef boost::filesystem::directory_iterator directory_iterator;
 
-        size_t search(const path &aPath) {
-            size_t counter = 0;
-            std::stack<path, std::vector<path>> folders;
-            folders.push(aPath);
-            while (!folders.empty()) {
-                auto aPath = folders.top();
-                folders.pop();
-                directory_iterator endIter;
-                directory_iterator dirIter(aPath);
-                for (; dirIter != endIter; ++dirIter) {
-                    auto currentPath = dirIter->path();
-                    auto status = dirIter->status();
-                    auto ftype = status.type();
-                    if (ftype == boost::filesystem::regular_file) {
-                        if (isValidFile(currentPath)) {
-                            update(currentPath, status);
-                            counter++;
-                        }
-                    } else if (ftype == boost::filesystem::directory_file) {
-                        if (isValidDir(currentPath)) {
-                            folders.push(currentPath);
-                        }
-                    } else {
-                        unexpected(currentPath);
-                    }
-                }
-            }
-            return counter;
-        }
-
-      protected:
-        virtual bool isValidDir(const path &aPath) = 0;
-        virtual bool isValidFile(const path &aPath) = 0;
-        virtual void update(const path &aPath,
-                            boost::filesystem::file_status &fs) = 0;
-        virtual void unexpected(const path &aPath) = 0;
-    };
-
-    template <typename Base> class SimpleDFSFinder : public Base {
-      public:
-        auto getData() { return Data; }
-
-      protected:
-        typedef typename Base::path path;
-        bool isValidDir(const path &aPath) {
-            const std::array<std::string, 1> excludedFolders = {{".git"}};
-            auto searchStr = aPath.extension().string();
-            return std::find(excludedFolders.begin(), excludedFolders.end(),
-                             searchStr) == excludedFolders.end();
-        };
-        bool isValidFile(const path &) { return true; };
-
-        void update(const path &aPath, boost::filesystem::file_status &fs) {
-            Data.emplace_back(
-                std::make_tuple(aPath.string(), aPath.stem().string(),
-                                aPath.extension().string(), fs.permissions(),
-                                boost::filesystem::last_write_time(aPath)));
-        };
-        void unexpected(const path &aPath){
-          std::cout << "How to handle this: " << aPath << std::endl;
-        };
-
-      private:
-        std::vector<Tools::EditedFileInfo> Data;
-    };
-}
-
-int main(int argc, char *argv[]) {
+int main(int, char *argv[]) {
     boost::filesystem::path aPath(argv[1]);
 
     {
         Timer timer;
-        Tools::SimpleDFSFinder<Tools::DFSFinder> finder;
+        Utils::SimpleFileSearch<Utils::DFSFileSearchBase> finder;
+        std::cout << "Number of file: " << finder.search(aPath) << std::endl;
+        std::cout << "Total time: " << timer.toc() / timer.ticksPerSecond()
+                  << " seconds" << std::endl;
+        // for (auto aFile : finder.getData()) {
+        //     std::cout << aFile << "\n";
+        // }
+    }
+
+    {
+        Timer timer;
+        Utils::SimpleFileSearch<Utils::BFSFileSearchBase> finder;
         std::cout << "Number of file: " << finder.search(aPath) << std::endl;
         std::cout << "Total time: " << timer.toc() / timer.ticksPerSecond()
                   << " seconds" << std::endl;
