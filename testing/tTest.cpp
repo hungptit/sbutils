aa5b98f2f03deade31033b7ff6b6920b75c7a4c9
Hung Dang <hungptit@gmail.com>
2016-02-21 18:58:59 -0500

    Update todo.md

--- testing/tTest.cpp
+++ testing/tTest.cpp
@@ -2,11 +2,11 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <set>
 #include <string>
 #include <tuple>
 #include <unordered_map>
-#include <set>
-#include <map>
 #include <unordered_set>
 #include <vector>
 
@@ -21,10 +21,10 @@
 #include "utils/BFSFileSearch.hpp"
 #include "utils/DFSFileSearch.hpp"
 #include "utils/FileSearch.hpp"
+#include "utils/FolderDiff.hpp"
 #include "utils/LevelDBIO.hpp"
 #include "utils/Timer.hpp"
 #include "utils/Utils.hpp"
-#include "utils/FolderDiff.hpp"
 
 #include <sstream>
 #include <string>
@@ -32,10 +32,9 @@
 
 #include "gtest/gtest.h"
 
-
 int main(int argc, char *argv[]) {
     boost::filesystem::path aPath((argc == 2) ? argv[1] : "./");
-    
+
     std::array<std::string, 1> stems = {{"foo"}};
     std::vector<std::string> exts = {{".txt", ".cmake", ".make", ".o", ".bin",
                                       ".internal", ".includecache", ".marks"}};
@@ -68,12 +67,16 @@ int main(int argc, char *argv[]) {
 
     {
         std::vector<std::string> stems = {{"CMakeLists"}};
-        std::vector<std::string> exts = {{".txt", ".cmake", ".make", ".o", ".bin",
-                                          ".internal", ".includecache", ".marks"}};
+        std::vector<std::string> exts = {{".txt", ".cmake", ".make", ".o",
+                                          ".bin", ".internal", ".includecache",
+                                          ".marks"}};
         utils::Timer timer;
         typedef utils::FileSearchBase<utils::DFSFileSearchBase> Boo;
-        typedef utils::BasicFileSearch<Boo, std::vector<std::string>, std::vector<std::string>> Foo;
-        typedef utils::BasicFileSearch<utils::DFSFileSearchBase, std::vector<std::string>, std::vector<std::string>> Hoo;
+        typedef utils::BasicFileSearch<Boo, std::vector<std::string>,
+                                       std::vector<std::string>> Foo;
+        typedef utils::BasicFileSearch<utils::DFSFileSearchBase,
+                                       std::vector<std::string>,
+                                       std::vector<std::string>> Hoo;
 
         // Foo finder(stems, exts);
         Foo finder(stems, exts);
@@ -82,47 +85,45 @@ int main(int argc, char *argv[]) {
                   << " seconds" << std::endl;
     }
 
-    {                    
-      typedef std::vector<utils::FileInfo> Container;
-      const std::string dataFile = ".database";
-      utils::Reader reader(dataFile);
-      utils::FolderDiff<Container> diff;
-      const std::string aKey("/local-ssd/sandbox/exportfcns");
+    {
+        typedef std::vector<utils::FileInfo> Container;
+        const std::string dataFile = ".database";
+        utils::Reader reader(dataFile);
+        utils::FolderDiff<Container> diff;
+        const std::string aKey("/local-ssd/sandbox/exportfcns");
 
-      typedef utils::FileSearchBase<utils::BFSFileSearchBase> FileSearch;
-      FileSearch finder;
+        typedef utils::FileSearchBase<utils::BFSFileSearchBase> FileSearch;
+        FileSearch finder;
 
-      // Find files
-      utils::Timer timer;
-      diff.find(finder, aKey);
-      std::cout << "Find time: " << timer.toc() / timer.ticksPerSecond()
-                << " seconds" << std::endl;
-      // utils::print(finder.getData());
-      
-      // Read data
-      timer.tic();
-      auto dict = diff.read(reader, aKey);
-      std::cout << "Read time: " << timer.toc() / timer.ticksPerSecond()
-                << " seconds" << std::endl;
-      // utils::print(dict);
-      
-      // Find the diff
-      timer.tic();
-      Container editedFiles, newFiles, deletedFiles;
-      std::tie(editedFiles, newFiles, deletedFiles) = diff.diff(finder.getData(), dict);   
-      std::cout << "Diff time: " << timer.toc() / timer.ticksPerSecond()
-                << " seconds" << std::endl;   
+        // Find files
+        utils::Timer timer;
+        diff.find(finder, aKey);
+        std::cout << "Find time: " << timer.toc() / timer.ticksPerSecond()
+                  << " seconds" << std::endl;
+        // utils::print(finder.getData());
 
-      std::cout << "---- Modified files: "
-                << editedFiles.size() << " ----\n";
-      utils::print(editedFiles);
-      
-      std::cout << "---- New files: "
-                << newFiles.size() << " ----\n";
-      utils::print(newFiles);
+        // Read data
+        timer.tic();
+        auto dict = diff.read(reader, aKey);
+        std::cout << "Read time: " << timer.toc() / timer.ticksPerSecond()
+                  << " seconds" << std::endl;
+        // utils::print(dict);
 
-      std::cout << "---- Deleted files: "
-                << deletedFiles.size() << " ----\n";
-      utils::print(deletedFiles);
+        // Find the diff
+        timer.tic();
+        Container editedFiles, newFiles, deletedFiles;
+        std::tie(editedFiles, newFiles, deletedFiles) =
+            diff.diff(finder.getData(), dict);
+        std::cout << "Diff time: " << timer.toc() / timer.ticksPerSecond()
+                  << " seconds" << std::endl;
+
+        std::cout << "---- Modified files: " << editedFiles.size() << " ----\n";
+        utils::print(editedFiles);
+
+        std::cout << "---- New files: " << newFiles.size() << " ----\n";
+        utils::print(newFiles);
+
+        std::cout << "---- Deleted files: " << deletedFiles.size() << " ----\n";
+        utils::print(deletedFiles);
     }
 }
--- todo.md
+++ todo.md
@@ -1,28 +1,40 @@
-# Parallelize the search algorithm #
-* Build database
-    * Given a root folders
-        * Explore the folders to k levels
-        * Each node (folder) will store
-            * Files at the root level.
-            * All children of that node.
-        * How to store the tree information?
-            * Node's name -> files and folder at root level.
-              Node -> files
-                   -> folders
-            * 
-            * 
-        * Detail implementation
-            * 
-* Search algorithm
-    * Get a list of folders which are stored in the database
-    * For each folder
-        * Get the database
-        * Get the list of files which has been changed using time stamp.
-        * This will be executed assynchrously.
-    * Search for each folder will be executed assynchronously and results will be merged at the end.
-    * Split results into three lists:
-        * Edited files
-        * New files.
-        * Deleted files.
-    
-    
+# Multi-threaded search algorithm #
+
+* Have functions which can
+    * Create a tree of folder hierarchy
+    * Serialize a folder tree (_fstructure)
+    * De-serialize a folder tree
+    * Serialize/de-serialize a list of folder paths (_fpaths).
+* Create baseline: Users will need to provide desire exploration level
+    * Use DFS algorithm.
+    * Only serialize file names at top level if this is not a leaf folder (at lowest level).
+    * Serialize all file names belong to the leaf folder.
+* How to load a baseline
+    * Linear search in a list of folder names (key name is info).
+        * If a given folder is in the list then load all data from the current vertex + it children.
+        * If a given folder is not in the list then we find the closest ancestor and load the data from that closest ancestor. If we could not find any then throw an error.
+        * Use task based parallelism approach + move semantic to speed up.
+* Search algorithm -> Use task based parallelism strategy.
+    * For each search folder get a sub-graph of folder users want to search.
+    * For each leaf folder
+        * Load the baseline.
+        * Collect all files information.
+        * Find the differences between baseline and current results.
+    * For each non-leaf folder
+        * Load the baseline
+        * Collect all files at top level
+        * Find the differences between baseline and current results.
+    * Combine obtained results at the main threads. We need 3 lists of added, modified, and deleted files.
+* Design
+    * We should be able to modify the search algorithm such that it can
+        * Copy change lists from source sandbox to destination sandbox.
+        * Find files in database given patterns. Shall we try vertical search engines (Lucene++)?
+    * Have a function which return the differences between two vectors. This should be a template function so we can test it separately.
+    * Get the improved version of viewer where users can give a folder that they want to locate.
+        * File information are stored into keys then we can use task based parallelism approach.
+        * Users can specified fllowing constraint: search folders, file extensions, file stems, and may be file patterns.
+
+* Todo
+    * Change commands to mlocate, mdiff, mupdatedb, mcopy with better help.
+    * Support JSON output format.
+    * Use cppformat instead of iostream.
