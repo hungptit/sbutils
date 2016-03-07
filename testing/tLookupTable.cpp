fed92e06e3a7c11632bab29cc7ee3eeb0832e909
Hung Dang <hungptit@gmail.com>
2016-03-07 04:22:53 -0500

    Add tLookupTable.cpp.

---
+++ testing/tLookupTable.cpp
@@ -0,0 +1,60 @@
+#include <array>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define BOOST_THREAD_VERSION 4
+#include "boost/config.hpp"
+#include "boost/filesystem.hpp"
+#include "boost/program_options.hpp"
+#include "boost/thread.hpp"
+#include "boost/thread.hpp"
+#include "boost/thread/future.hpp"
+
+#include "utils/BFSFileSearch.hpp"
+#include "utils/DFSFileSearch.hpp"
+#include "utils/FileSearch.hpp"
+#include "utils/FolderDiff.hpp"
+#include "utils/LevelDBIO.hpp"
+#include "utils/LookupTable.hpp"
+#include "utils/Timer.hpp"
+#include "utils/Utils.hpp"
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+
+
+template <typename Container> auto createTestData() {
+    Container data = {std::make_tuple("foo", 1), std::make_tuple("boo", 2),
+                      std::make_tuple("foo", 2), std::make_tuple("foo", 3)};
+    return data;
+}
+
+TEST(BasicTests, Positive) {
+    using Container = std::vector<std::tuple<std::string, size_t>>;
+    auto data = createTestData<Container>();
+
+    {
+        utils::LookupTable<Container> table(data);
+        table.disp();
+        auto results = table.getIdx("foo");
+        auto begin = std::get<0>(results), end = std::get<1>(results);
+        std::cout << "--- Results ---\n";
+        std::for_each(begin, end, [](auto &val) { std::cout << val << "\n"; });
+
+        std::cout << table.get(3) << "\n";
+        std::cout << table.get(2) << "\n";
+        std::cout << table.get(0) << "\n";
+    }
+}
