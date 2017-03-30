c10203478a1a46ce48e7e0bfdece81474ed23890
Hung Dang <hdang@athenahealth.com>
2017-03-30 19:50:12 -0400

    Have a basic version of mutest. Need to add a decent parser so that we can display a summary table on screen.

--- commands/mutest.cpp
+++ commands/mutest.cpp
@@ -15,9 +15,80 @@
 #include "tbb/parallel_invoke.h"
 #include "tbb/tbb.h"
 
+#include "cereal/archives/binary.hpp"
+#include "cereal/archives/json.hpp"
+#include "cereal/archives/portable_binary.hpp"
+#include "cereal/archives/xml.hpp"
+#include "cereal/types/array.hpp"
+#include "cereal/types/chrono.hpp"
+#include "cereal/types/deque.hpp"
+#include "cereal/types/string.hpp"
+#include "cereal/types/vector.hpp"
+
 #include "spdlog/spdlog.h"
 
 auto console = spdlog::stdout_color_mt("console");
+
+namespace {
+    struct TestData {
+        TestData() = default;
+
+        TestData(const std::string &testName, const int errcode, const std::string &outputLog,
+                 const std::string &errorLog)
+            : UnitTest(testName), ErrorCode(errcode), OutputLog(outputLog),
+              ErrorLog(errorLog){};
+
+        std::string UnitTest;
+        int ErrorCode;
+        std::string OutputLog;
+        std::string ErrorLog;
+
+        template <typename Archive> void serialize(Archive &ar) {
+            ar(cereal::make_nvp("Unit test", UnitTest),
+               cereal::make_nvp("Error code", ErrorCode),
+               cereal::make_nvp("Test log", OutputLog),
+               cereal::make_nvp("Error message", ErrorLog));
+        }
+    };
+
+    template <typename OutputArchive>
+    void writeToFile(
+        const tbb::concurrent_vector<std::tuple<std::string, sbutils::CommandOutput>> &results,
+        const std::string &outputFile) {
+        std::vector<TestData> data(results.size());
+
+        // Transform the tuple to struct so that we can have more descriptive output.
+        std::transform(results.begin(), results.end(), data.begin(), [](auto item) {
+            auto value = std::get<1>(item);
+            return TestData(std::get<0>(item), std::get<2>(value), std::get<0>(value),
+                            std::get<1>(value));
+        });
+
+        // Output to file.
+        std::stringstream ss;
+        {
+            OutputArchive archive(ss);
+            archive(cereal::make_nvp("Test data", data));
+        }
+
+        std::ofstream ofs;
+        ofs.open(outputFile, std::ofstream::out);
+        ofs << ss.str();
+        ofs.close();
+
+		// Log to the console
+		console->info("Write all test results to \"{0}\"", outputFile);
+    }
+
+	
+	
+	auto parseTestLog(const std::string &output) {
+		std::tuple<bool, int, int, int> results;
+		
+		return results;
+	}
+	
+}
 
 int main(int argc, char *argv[]) {
     namespace po = boost::program_options;
@@ -28,8 +99,8 @@ int main(int argc, char *argv[]) {
     bool failedOnly = true;
     bool nocoverage = true;
 
-	std::string format;
-	std::string outputFile;
+    std::string outputFile;
+	const std::string defaultOutputFile("output.json");
 	
     // clang-format off
     desc.add_options()
@@ -38,8 +109,7 @@ int main(int argc, char *argv[]) {
 		("nocoverage", po::value<bool>(&nocoverage)->default_value(true), "Run code coverage.")
 		("nodbh", po::value<bool>(&nodbh)->default_value(true), "Do not use database")
 		("failedonly", po::value<bool>(&failedOnly)->default_value(true), "only dislay failed tests")
-		("output-format,f", po::value<std::string>(&format)->default_value("stdout"), "Select the output format")
-		("output-file,o", po::value<std::string>(&outputFile)->default_value("output.dat"), "Output file name")
+		("output-file,o", po::value<std::string>(&outputFile)->default_value(defaultOutputFile), "Output file name")
 		("tests,t", po::value<std::vector<std::string>>(&unitTests), "Test modules.");
     // clang-format on
 
@@ -55,12 +125,11 @@ int main(int argc, char *argv[]) {
     }
 
     if (unitTests.empty()) {
-        fmt::print("You need to provide a test module or test point!\n");
+        console->critical("You need to provide a test module or test point!\n");
         return EXIT_SUCCESS;
     }
 
     std::vector<std::string> utestArgs;
-
     bool verbose = vm.count("verbose");
 
     if (nocoverage) {
@@ -88,16 +157,16 @@ int main(int argc, char *argv[]) {
         results.push_back(std::make_tuple(unitTests[index], outputs));
         console->info("Finish {0} in {1} seconds.", unitTests[index],
                       timer.toc() / timer.ticksPerSecond());
+
+		// Parse the test results
+		
     };
 
     int size = static_cast<int>(unitTests.size());
     tbb::parallel_for(0, size, 1, testObj);
 
-    if (verbose) {
-        std::for_each(results.begin(), results.end(),
-                      [&writer](auto item) { writer << std::get<0>(std::get<1>(item)); });
-    }
+    // Write results to an output file
+    writeToFile<cereal::JSONOutputArchive>(results, outputFile);
 
-    fmt::print("{}\n", writer.str());
     return EXIT_SUCCESS;
 }
