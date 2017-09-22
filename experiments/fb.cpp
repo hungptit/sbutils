bad768734211555044d12995de1ccd50692e4859
Hung Dang <hdang@athenahealth.com>
2017-09-21 22:35:10 -0400

    Add a simple benchmark for fbstring.

--- experiments/fb.cpp
+++ experiments/fb.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include <fstream>
 #include "folly/FBString.h"
+#include "folly/FBVector.h"
 #include "fmt/format.h"
 
 #include <cereal/types/memory.hpp>
@@ -15,9 +16,9 @@ namespace cereal
 	using CharT = char;
 	
   //! Serialization for basic_string types, if binary data is supported
-  template<class Archive> inline
+	template<typename Archive, typename CharT, typename Allocator> inline
   typename std::enable_if<traits::is_output_serializable<BinaryData<char>, Archive>::value, void>::type
-  CEREAL_SAVE_FUNCTION_NAME(Archive & ar, folly::fbstring const & str)
+	CEREAL_SAVE_FUNCTION_NAME(Archive & ar, folly::basic_fbstring<CharT, Allocator> const & str)
   {
     // Save number of chars + the data
     ar( make_size_tag( static_cast<size_type>(str.size()) ) );
@@ -25,26 +26,58 @@ namespace cereal
   }
 
   //! Serialization for basic_string types, if binary data is supported
-  template<class Archive> inline
+	template<class Archive, typename CharT, typename Allocator> inline
   typename std::enable_if<traits::is_input_serializable<BinaryData<char>, Archive>::value, void>::type
-  CEREAL_LOAD_FUNCTION_NAME(Archive & ar, folly::fbstring & str)
+	CEREAL_LOAD_FUNCTION_NAME(Archive & ar, folly::basic_fbstring<CharT, Allocator> & str)
   {
     size_type size;
-    ar( make_size_tag( size ) );
+
+	ar( make_size_tag( size ) );
     str.resize(static_cast<std::size_t>(size));
     ar( binary_data( const_cast<CharT *>( str.data() ), static_cast<std::size_t>(size) * sizeof(CharT) ) );
   }
 } // namespace cereal
 
 
-int main() {
-	folly::fbstring str("Hello");
-	fmt::print("{}\n", str.data());
-	std::unordered_set<folly::fbstring> dict;
+#include "celero/Celero.h"
 
-	std::ofstream os("out.cereal", std::ios::binary);
-	cereal::BinaryOutputArchive archive( os );
-	archive(str);
+constexpr int NumberOfSamples = 200000;
+constexpr int NumberOfIterations = 10;
 
-	
+std::string aLine("fdsf ggsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssfdgfd  gfdg f gfdgdf gd ");
+folly::fbstring aLinefb(aLine);
+
+const char pattern[] = {"Hello fdsf dsd"};
+
+std::string stdpattern(pattern);
+folly::fbstring fbpattern(pattern); 
+
+template<typename String>
+bool search(const String &str, const String &pattern) {
+	return str.find(pattern) != String::npos;
 }
+
+CELERO_MAIN
+
+BASELINE(search, std_string, NumberOfSamples, NumberOfIterations) {
+    celero::DoNotOptimizeAway(search<std::string>(aLine, stdpattern));
+}
+
+BENCHMARK(search, fbstring1, NumberOfSamples, NumberOfIterations) {
+    celero::DoNotOptimizeAway(search<folly::fbstring>(aLinefb, pattern));
+}
+
+BENCHMARK(search, fbstring2, NumberOfSamples, NumberOfIterations) {
+    celero::DoNotOptimizeAway(search<folly::fbstring>(aLinefb, fbpattern));
+}
+
+
+// int main() {
+// 	folly::fbstring str("Hello");
+// 	fmt::print("{}\n", str.data());
+// 	std::unordered_set<folly::fbstring> dict;
+
+// 	std::ofstream os("out.cereal", std::ios::binary);
+// 	cereal::BinaryOutputArchive archive( os );
+// 	archive(str);
+// }
