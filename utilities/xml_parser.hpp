e49a6a540fb79967d4deab4dd85ab1bffbbd748e
Hung Dang <hungptit@gmail.com>
2017-11-17 19:49:43 -0500

    Create a separate project for jenkins related functionality.

---
+++ utilities/xml_parser.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace test {
+
+	class CloverParser {
+	public:
+		// Return the clover code coverage information.
+		void parse() {
+			
+		}
+		
+		void parse_line() {
+		
+		}
+
+		void parse metrics() {
+			
+		}
+
+		void parse_class () {
+			
+		}
+
+		void parse_file() {
+		
+		}
+
+		void parse_pacakge() {
+		
+		}
+
+		void parse_project() {
+		
+		}
+	private:
+		
+	};
+
+	class JunitParser {
+		void parse() {
+			
+		}
+	};
+}
